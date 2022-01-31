dd0161ab6a01fa5bb9615f2e371c4d11f490343a
Simone Montalbano <simone.montalbano@pm.me>
2022-01-31 18:16:57 +0100

    update

--- penncnv_pipeline/01_preprocess.R
+++ penncnv_pipeline/01_preprocess.R
@@ -1,4 +1,3 @@
-
 # CHANGE THIS if batches should be larger or smaller #
 bs <- 2000
 
@@ -7,32 +6,32 @@ args <- commandArgs(trailingOnly=TRUE)
 
 # read samples list
 slistp <- paste0(args[1], "/samples_list.txt")
-if (!file.exists(slistp)) error("Samples list file not found")
+if (!file.exists(slistp)) stop("Samples list file not found")
 
 if (args[2] == 1) {
   slist <- fread(args[1])
   if (!all(c("sample_ID", "file_path") %in% colnames(slist)))
-    error("Wrong columns in samples list file")
+    stop("Wrong columns in samples list file")
 
-  if (any(duplicated(slist[, sample_ID]))) error("Duplicated samples ID!")
+  if (any(duplicated(slist[, sample_ID]))) stop("Duplicated samples ID!")
 
   # test 25% random files
   if (!all(file.exists(sample(slist[, file_path], nrow(slist)*0.25))))
-    error("Some intensity files are missing!")
+    stop("Some intensity files are missing!")
 
   # test 100 random files format
   for (f in sample(slist[, file_path], 100)) {
     if!(file.exists(f))
-      error(paste0("Intensity file missing! File: ", f))
+      stop(paste0("Intensity file missing! File: ", f))
     # read only the first 20 lines
     tmp <- fread(paste0("head ", f, " -n20"), skip = "Position")
     # if some lines are missing they were skipped by fread()
     if (nrow(tmp) < 19)
-      error(paste0("Intensity file in the wrong format. Long header not removed? File: ", f))
+      stop(paste0("Intensity file in the wrong format. Long header not removed? File: ", f))
     # check is 'Sample ID' column is present, if yes check content
     if (`Sample ID` %in% colnames(tmp)) {
       tmp <- fread(f, select = "Sample ID")[, `Sample ID`]
-      if (!all(tmp == tmp[1])) error(paste0("Multiple samples per intensity file! File: ", f))
+      if (!all(tmp == tmp[1])) stop(paste0("Multiple samples per intensity file! File: ", f))
     }
   }
 }
@@ -54,7 +53,7 @@ if (!"batch" %in% colnames(slist)) {
 
 if ("batch" %in% colnames(slist))
   if (!all(is.integer(slist[, batch])) | is.na(is.integer(slist[, batch])))
-    error("Batch column is in the wrong format!")
+    stop("Batch column is in the wrong format!")
 
 # overwrite samples list and write individual batches files
 fwrite(slist, args[1], sep = "\t")
@@ -65,3 +64,4 @@ dir.create(lf)
 for (i in unique(slist[, batch]))
   fwrite(slist[batch := i, .(file_path)], paste0(lf, "/batch", i,".txt"), col.names = F)
 
+message("Step 1 done! ", i, " batches will be used.")

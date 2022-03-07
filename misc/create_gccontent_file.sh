a5f2dd4292c599481f4fdc17508e3833118ebd17
Simone Montalbano <48947872+SinomeM@users.noreply.github.com>
2022-03-07 13:16:29 +0000

    Create create_gccontent_file.sh

---
+++ misc/create_gccontent_file.sh
@@ -0,0 +1,6 @@
+# GC content files in the correct for hg18 and hg19 are provided here and in the PennCNV
+# official repository as well. There, it is also available the file for hg38.
+# See https://github.com/SinomeM/IBPcnv/tree/master/gcfile and
+# https://github.com/WGLab/PennCNV/tree/master/gc_file
+
+# A more general script to generate those files will be provided soon.

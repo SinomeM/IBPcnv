382d39eb642246eb8f26bd1f5cad7734d85aa9ff
Simone Montalbano <simone.montalbano@pm.me>
2022-05-17 15:58:08 +0200

    up

--- penncnv_pipeline/02_select_SNPs.R
+++ penncnv_pipeline/02_select_SNPs.R
@@ -4,8 +4,11 @@ setwd(args[1])
 
 # Downloads SNPs list from HRC
 # only strictly biallelic SNPs, with at least 1 alternate allele count, and only known (no ".")
-system(paste0("bcftools view -m2 -M2 -v snps -c10:minor -k -Ou ftp://ngs.sanger.ac.uk/production/hrc/HRC.r1-1/HRC.r1-1.GRCh37.wgs.mac5.sites.vcf.gz | bcftools filter -i 'AF > ", args[2], "' -Oz > hg19_snp_list.vfc.gz"))
-system("tabix hg19_snp_list.vfc.gz")
+if (args[3] == "hg19")
+  system(paste0("bcftools view -m2 -M2 -v snps -c10:minor -k -Ou ftp://ngs.sanger.ac.uk/production/hrc/HRC.r1-1/HRC.r1-1.GRCh37.wgs.mac5.sites.vcf.gz | bcftools filter -i 'AF > ", args[2], "' -Oz > hg19_snp_list.vfc.gz"))
+
+system(paste0("tabix ", args[3], "_snp_list.vfc.gz"))
+
 
 
 library(VariantAnnotation); library(data.table)
@@ -15,9 +18,9 @@ slistp <- paste0 (args[1], "/samples_list.txt")
 tmp <- fread (slistp[1, file_path], skip = "Position")[, .(Name, Chr, Position)]
 
 # Read VCF
-fl <- ("hg19_snp_list.vfc.gz")
+fl <- (paste0(args[3], "_snp_list.vfc.gz"))
 message ("Loading SNPS info into R")
-vcf <- rowRanges(readVcf(fl, "hg19"))
+vcf <- rowRanges(readVcf(fl, args[3]))
 dt <- as.data.table( cbind(as.data.frame(ranges (vcf)), as.data.frame(seqnames (vcf))) ) [, .(value, start, names)]
 colnames (dt) <- c("chr", "pos", "SNP_ID")
 

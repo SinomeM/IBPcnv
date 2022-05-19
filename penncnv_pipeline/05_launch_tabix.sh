102e32bc1722fc95ee80ee4989156a8ad37f4e1d
Simone Montalbano <simone.montalbano@pm.me>
2022-05-19 18:47:43 +0200

    up

--- penncnv_pipeline/05_launch_tabix.sh
+++ penncnv_pipeline/05_launch_tabix.sh
@@ -9,6 +9,10 @@ LANG=C
 
 tail -n +2 $snp | sort -f -k1 -t '	' > ${snp}.sorted
 
-for i in ...
-  bash $ibp/penncnvpipeline/05_1_tabix_indexing.sh $INTFILE $TABIXFILE $wkdir $ibpcnv
+for i in $( seq 2 $( wc -l $samps | cut -d' ' -f1 ) ); do
+
+  in=$( sed -n "$i"p $samps | cut -f2 )
+  out=$( sed -n "$i"p $samps | cut -f4 )
+  sbatch $ibp/penncnvpipeline/05_1_tabix_indexing.sh $in $out $wkdir $ibpcnv
+
 done

3d1ff45498c3cb87ffcfecf146e2b5912c6e85a5
Simone Montalbano <simone.montalbano@pm.me>
2022-05-20 09:40:44 +0200

    up

--- penncnv_pipeline/05_launch_tabix.sh
+++ penncnv_pipeline/05_launch_tabix.sh
@@ -13,6 +13,6 @@ for i in $( seq 2 $( wc -l $samps | cut -d' ' -f1 ) ); do
 
   in=$( sed -n "$i"p $samps | cut -f2 )
   out=$( sed -n "$i"p $samps | cut -f4 )
-  sbatch $ibpcnv/penncnvpipeline/05_1_tabix_indexing.sh $in $out $wkdir $ibpcnv
+  sbatch $ibpcnv/penncnv_pipeline/05_1_tabix_indexing.sh $in $out $wkdir $ibpcnv
 
 done

42fe403e6aebb3387b7c9205135fe13c656dacfb
Simone Montalbano <48947872+SinomeM@users.noreply.github.com>
2022-06-14 16:44:06 +0200

    Update 03_penncnv_pipeline.sh

--- penncnv_pipeline/03_penncnv_pipeline.sh
+++ penncnv_pipeline/03_penncnv_pipeline.sh
@@ -6,6 +6,8 @@ scripts=${2} # the location of IBPcnv repo clone
 batches=${3}
 hg=${4}
 
+cd ${wkdir}/logs #unelegant solution to put PBS output in logs
+
 snpposfile=${wkdir}/snppos_filtered.txt
 gcmodel=${wkdir}/gcmodel.txt
 gccont=${scripts}/gcfile/${hg}gc.txt
@@ -20,15 +22,14 @@ echo -e "\nCreating GC content model"
 # uncomment and edit this version of the command to use a SLURM account
 # srun --account=$s_acc -c2 --mem=50g --time 00:30:00 \
 #   $simg cal_gc_snp.pl $gccont $snpposfile --output $gcmodel
-srun -c1 --mem=10g --time 00:30:00 \
+qsub -c1 --mem=10g --time 00:30:00 \
   $simg cal_gc_snp.pl $gccont $snpposfile --output $gcmodel
 
 # Main loop #
 for wv in $( seq 1 $batches ); do
 
   echo -e "\nLaunching the calling pipeline in batch n${wv}\n"
-  # batch number and SLURM account are passed to the script
-  sbatch ${scripts}/penncnv_pipeline/03_1_per_wave.sh $wkdir $scripts $wv
+  qsub ${scripts}/penncnv_pipeline/03_1_per_wave.sh $wkdir $scripts $wv
   sleep 2
 
 done

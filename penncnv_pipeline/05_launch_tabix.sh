5d8b759d03ec04f327408cfeec3c7ed34468c5d5
Simone Montalbano <48947872+SinomeM@users.noreply.github.com>
2022-11-01 20:50:23 +0100

    added messages

--- penncnv_pipeline/05_launch_tabix.sh
+++ penncnv_pipeline/05_launch_tabix.sh
@@ -7,7 +7,11 @@ samps=${wkdir}/samples_list.txt
 
 LANG=C
 
+echo "Sorting SNPs list"
+
 tail -n +2 $snp | sort -f -k1 -t '	' > ${snp}.sorted
+
+echo "Submitting jobs"
 
 for i in $( seq 2 $( wc -l $samps | cut -d' ' -f1 ) ); do
 
